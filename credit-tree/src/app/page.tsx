4f4c1100e625cca28c39761fb80b3e190d05a3e0
Asher De Souza <asher.desouza@clearscore.com>
2025-06-04 12:45:23 +0100

    adding the home page and a link to Get Started

--- credit-tree/src/app/page.tsx
+++ credit-tree/src/app/page.tsx
@@ -1,5 +1,28 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>CreditTree</div>;
+  return (
+    <div className={styles.container}>
+      <div className={`fontPacifico ${styles.title}`}>CreditTree</div>
+      <Link href="/get-started" className={`fontPaytone ${styles.getStarted}`}>
+        GET STARTED
+      </Link>
+      <Image
+        className={styles.sapling}
+        src="/resources/sapling.png"
+        width={97}
+        height={150}
+        alt="sapling"
+      />
+      <Image
+        className={styles.ground}
+        src="/resources/ground.png"
+        width={3000}
+        height={761}
+        alt="ground"
+      />
+    </div>
+  );
 }
