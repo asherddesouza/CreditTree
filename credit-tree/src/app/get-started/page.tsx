ee6daa615c6cfede43b4bf04891a728880981846
Asher De Souza <asher.desouza@clearscore.com>
2025-06-07 11:51:20 +0100

    added main profile page

--- credit-tree/src/app/get-started/page.tsx
+++ credit-tree/src/app/get-started/page.tsx
@@ -4,39 +4,44 @@ import Link from "next/link";
 
 export default function GetStarted() {
   return (
-    <div className={styles.container}>
-      <div className={`fontPacifico ${styles.title}`}>Welcome!</div>
-      <div className={`fontPavanam ${styles.subtitle}`}>
-        If you’ve already got an account, log in to continue your journey!
+    <div className={styles.backgroundContainer}>
+      <div className={styles.container}>
+        <div className={`fontPacifico ${styles.title}`}>Welcome!</div>
+        <div className={`fontPavanam ${styles.subtitle}`}>
+          If you’ve already got an account, log in to continue your journey!
+        </div>
+        <Link
+          href="/get-started/login"
+          className={`fontPaytone ${styles.login}`}
+        >
+          LOGIN
+        </Link>
+        <div className={`fontPacifico ${styles.dividerContainer}`}>
+          <div className={styles.dividerLine} />
+          <div className={`fontPavanam ${styles.divider}`}>or</div>
+          <div className={styles.dividerLine} />
+        </div>
+        <Link
+          href="/get-started/register"
+          className={`fontPaytone ${styles.signup}`}
+        >
+          REGISTER
+        </Link>
+        <Image
+          className={styles.sapling}
+          src="/resources/sapling.png"
+          width={97}
+          height={150}
+          alt="sapling"
+        />
+        <Image
+          className={styles.ground}
+          src="/resources/ground.png"
+          width={3000}
+          height={761}
+          alt="ground"
+        />
       </div>
-      <Link href="/get-started/login" className={`fontPaytone ${styles.login}`}>
-        LOGIN
-      </Link>
-      <div className={`fontPacifico ${styles.dividerContainer}`}>
-        <div className={styles.dividerLine} />
-        <div className={`fontPavanam ${styles.divider}`}>or</div>
-        <div className={styles.dividerLine} />
-      </div>
-      <Link
-        href="/get-started/register"
-        className={`fontPaytone ${styles.signup}`}
-      >
-        REGISTER
-      </Link>
-      <Image
-        className={styles.sapling}
-        src="/resources/sapling.png"
-        width={97}
-        height={150}
-        alt="sapling"
-      />
-      <Image
-        className={styles.ground}
-        src="/resources/ground.png"
-        width={3000}
-        height={761}
-        alt="ground"
-      />
     </div>
   );
 }
---
+++ credit-tree/src/app/profile/delete-account/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function DeleteAccount() {
+  return <div className={styles.container}></div>;
+}
