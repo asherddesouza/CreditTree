794adfd092239ffcb7c7ddf94024233a1ee20fd8
Asher De Souza <asher.desouza@clearscore.com>
2025-07-13 22:06:08 +0100

    fixed tests

--- credit-tree/src/app/profile/change-password/page.test.tsx
+++ credit-tree/src/app/profile/change-password/page.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../../src/app/profile/change-password/page.client";
+import Page from "./page.client";
 
 describe("Edit Profile", () => {
   it("renders the page correctly", () => {
--- credit-tree/src/app/profile/delete-account/page.test.tsx
+++ credit-tree/src/app/profile/delete-account/page.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Page from "../../src/app/profile/delete-account/page.client";
+import Page from "./page.client";
 
 describe("Delete Account", () => {
   it("renders the page correctly", () => {
