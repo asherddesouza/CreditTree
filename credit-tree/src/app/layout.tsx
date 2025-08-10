ec91ae16814ee35e4cc20cbda33c86d90d9e79ed
Asher De Souza <asher.desouza@clearscore.com>
2025-08-10 22:41:20 +0100

    fixing css issue

--- credit-tree/src/app/layout.tsx
+++ credit-tree/src/app/layout.tsx
@@ -2,19 +2,22 @@ import type { Metadata } from "next";
 import { Pacifico, Pavanam, Paytone_One } from "next/font/google";
 import "./globals.css";
 
-export const pacifico = Pacifico({
+const pacifico = Pacifico({
   weight: "400",
   variable: "--font-pacifico",
+  subsets: ["latin"],
 });
 
-export const pavanam = Pavanam({
+const pavanam = Pavanam({
   weight: "400",
   variable: "--font-pavanam",
+  subsets: ["latin"],
 });
 
-export const paytone = Paytone_One({
+const paytone = Paytone_One({
   weight: "400",
   variable: "--font-paytone",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
